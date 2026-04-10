e389535d380b69d7408dce691216efeef2a546e6
HENRIQUE PRATES CARDOSO <henrique.cardoso@poupex.com.br>
2026-04-10 13:51:40 -0300

    Aula 04 - Cop - Projeto002

--- projeto002/pages/views.py
+++ projeto002/pages/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-def home(request):
-    return HttpResponse('Olá, Django!')
+""" def home(request):
+    return HttpResponse('Olá, Django!') """
 def about(request):
     return HttpResponse('Página Sobre')
+
+def home(request):
+    return render(
+        request,
+        'pages/home.html',
+        {'titulo': 'Página Inicial'}
+    )
