9f7c00b577706a6e403d2ae67df27243c1262108
Henirque Cardoso <henrique.cardoso@poupex.com.br>
2026-04-24 17:58:47 -0300

    django admin

--- projeto002/pages/admin.py
+++ projeto002/pages/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Page, Post
 
 # Register your models here.
+
+@admin.register(Page)
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'criada_em')
+    search_fields = ('titulo',)
+    list_filter = ('criada_em'),
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'titulo', 'criada_em')
+    search_fields = ('titulo',)
+    list_filter = ('criada_em'),
