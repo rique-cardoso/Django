26291ba533c4f6917e30f5ff9896421e93bd6c7b
Henirque Cardoso <henrique.cardoso@poupex.com.br>
2026-05-07 17:47:18 -0300

    iniciando configurações de login

--- projeto002/core/settings.py
+++ projeto002/core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pages',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/posts/'
+LOGIN_REDIRECT_URL = '/login/'
